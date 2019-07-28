9876f02b79bcdc093f4fa70a924d26e6ab5fb65c
Ulrich Kramer <ulrich.kramer@web.de>
2019-07-28 12:44:36 +0200

    Remove convertion to arrays

--- plugins/java.rb
+++ plugins/java.rb
@@ -9,37 +9,31 @@ module Java
   include Rmk::Tools
 
   def javac(files, jarfiles, _options = {})
-    result = job('classes', files + jarfiles) do
+    job('classes', files, jarfiles) do
       classes_dir = File.join(build_dir, 'classes')
       FileUtils.rm_rf(classes_dir)
       FileUtils.mkdir_p(classes_dir)
-      jarfiles = jarfiles.map(&:result)
+      jarfiles = jarfiles.to_a.map(&:result)
       files = files.map(&:result)
       system("javac -cp #{jarfiles.join(':')} -d #{classes_dir} #{files.join(' ')}")
       Dir.glob(File.join(classes_dir, '**/*.class'))
     end
-    [result]
   end
 
   def jar(name, classfiles, resourcefiles = [], _options = {})
-    result = job(name + '.jar', classfiles + resourcefiles) do
+    job(name + '.jar', classfiles, resourcefiles) do
       lib_dir = File.join(build_dir, 'lib')
       result = File.join(lib_dir, name + '.jar')
       classes_dir = File.join(build_dir, 'classes')
       FileUtils.mkdir_p(lib_dir)
-      classfiles = classfiles.map(&:result).flatten
+      classfiles = classfiles.to_a.map(&:result).flatten
       file = Tempfile.new('jar')
-      begin
-        classfiles.each do |cls|
-          file.puts("-C #{classes_dir} #{File.relative_path_from(cls, classes_dir)}")
-        end
-        file.close
-        system("jar cf #{result} @#{file.path}")
-      ensure
-        # file.unlink
+      classfiles.each do |cls|
+        file.puts("-C #{classes_dir} #{File.relative_path_from(cls, classes_dir)}")
       end
-      [result]
+      file.close
+      system("jar cf #{result} @#{file.path}")
+      result
     end
-    [result]
   end
 end
